13106eefba4f205651a56bfdcae65aa430fe1e6a
sunabove <sunabove@nate.com>
2022-01-31 08:26:43 +0900

    **

--- data_manage.py
+++ data_manage.py
@@ -1,1 +1,21 @@
+from ftplib import FTP
+line = "*"*40
+line2 = f"\n{line}"
+
+print( line2 )
 print( f"Hello...." )
+
+host = "localhost"
+user = "icarus"
+passwd = "vmffotvha123!"
+ftp = FTP( host )
+ftp.login( user, passwd )
+print( "login_success" )
+
+print( line2 )
+print( "LIST files ....." )
+list = ftp.retrlines('LIST') 
+print( list )
+
+print( line2 )
+print( "Good bye!" )
